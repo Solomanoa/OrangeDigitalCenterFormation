0c092f1f9756fd44c925cb381d075f787c803b85
Manoa <andrianavalona0001@gmail.com>
2025-05-22 10:48:48 +0300

    Ajout du front ract

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Activation de la validation
+  // Activer CORS pour React
+  app.enableCors({
+    origin: 'http://localhost:3001', // Port par défaut de React
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Supprime les propriétés non définies dans les DTOs
-      forbidNonWhitelisted: true, // Rejette les requêtes avec des propriétés non définies
-      transform: true, // Transforme automatiquement les types
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   
   await app.listen(3000);
+  console.log('Backend NestJS running on http://localhost:3000');
 }
 bootstrap();
